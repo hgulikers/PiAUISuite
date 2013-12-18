2f15af79ad45b84b26806eeee0ca7f8b02798045
Harrie Gulikers <hgulikers@me.com>
2013-12-18 19:39:04 +0100

    Update voicecommand.cpp

--- VoiceCommand/voicecommand.cpp
+++ VoiceCommand/voicecommand.cpp
@@ -1,5 +1,7 @@
 #include "voicecommand.h"
 
+# HG: changed sample frequency from 1600 to 11025 Hz for support with Philips ToU webcam (USB audio input)
+
 using namespace std;
 using namespace boost;
 
